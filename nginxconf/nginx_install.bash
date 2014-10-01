822aa041b2f8e7466ac38872748f5ad49d0ceb8b
juno <juno@krasava.fi>
2014-10-01 05:10:27 +0300

    prod

--- nginxconf/nginx_install.bash
+++ nginxconf/nginx_install.bash
@@ -70,6 +70,9 @@ common2
 common3
 ./configure --prefix=/home/juno/git/nginxconf/nginxconf/nginx --user=juno --group=juno --conf-path=conf/common3.conf --with-http_gzip_static_module --with-http_geoip_module
 
+
+common4
+./configure --prefix=/home/juno/git/nginxconf/nginxconf/nginx --user=juno --group=juno --conf-path=conf/common3.conf --with-http_gzip_static_module --with-http_geoip_module
  
 
 
